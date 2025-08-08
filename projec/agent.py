daff79b16e18b07e92ec310721e0e3acdc935889
LUCIFER <96574607+pyprojectpi@users.noreply.github.com>
2025-08-08 15:49:13 +0530

    Add files via upload

---
+++ projec/agent.py
@@ -0,0 +1,47 @@
+from dotenv import load_dotenv
+
+from livekit import agents
+from livekit.agents import AgentSession, Agent, RoomInputOptions
+from livekit.plugins import (
+    google,
+    noise_cancellation,
+)
+import promt
+from promt import Agent_Instructions,Agent_Response 
+
+load_dotenv()
+
+
+class Assistant(Agent):
+    def __init__(self) -> None:
+        super().__init__(instructions=Agent_Instructions)
+
+
+async def entrypoint(ctx: agents.JobContext):
+    session = AgentSession(
+    llm=google.beta.realtime.RealtimeModel(
+        model="gemini-2.0-flash-exp",
+        voice="Puck",
+        temperature=0.8,
+        instructions=Agent_Instructions,
+    ),
+)
+
+    await session.start(
+        room=ctx.room,
+        agent=Assistant(),
+        room_input_options=RoomInputOptions(
+            # LiveKit Cloud enhanced noise cancellation
+            # - If self-hosting, omit this parameter
+            # - For telephony applications, use `BVCTelephony` for best results
+            noise_cancellation=noise_cancellation.BVC(),
+        ),
+    )
+
+    await session.generate_reply(
+        instructions=Agent_Response,
+    )
+        
+
+if __name__ == "__main__":
+    agents.cli.run_app(agents.WorkerOptions(entrypoint_fnc=entrypoint))
---
+++ projec/promt.py
@@ -0,0 +1,5 @@
+Agent_Instructions = '''You are Jarvis, my friendly and helpful AI assistant.  
+Always reply warmly, like you are having a natural conversation,  
+but still give clear and accurate information.'''
+
+Agent_Response = '''Always end your response with the word "SIR".'''
